8b589b58a98d4c146703ee558118eb43dd78f2b1
khanmaazm <64457110+khanmaazm@users.noreply.github.com>
2021-01-31 03:40:15 +0400

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 import flask
 
 app = flask.Flask(__name__)
-@app.route('/', methods = ['GET'])
-def home():    
+
+@app.route('/', methods = ['GET'])
+def home():    
    return "Default Home Function Return"
 
-@app.route('/second', methods = ['GET'])
-def home():    
+
+
+@app.route('/second', methods = ['GET'])
+def home():    
    return "Second Function Return"
 
-app.run()
+app.run()
