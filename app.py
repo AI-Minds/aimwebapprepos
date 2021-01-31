0bab6f3f7927fe5125aba6bfe726f5951130d843
khanmaazm <64457110+khanmaazm@users.noreply.github.com>
2021-01-31 04:33:21 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import flask
+app = flask.Flask(__name__)
+@app.route('/')
+def home():
+   return "Default Home Function Return"
+@app.route('/second', methods = ['GET'])
+def home2():
+   return "Second Function Return"
+app.run()
